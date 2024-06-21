5faaa87d090623063173253eba1e8947e94a2e7e
Muhammad Qasim <qasimvirk90@gmail.com>
2024-06-21 15:01:51 +0500

    username

--- app.js
+++ app.js
@@ -91,7 +91,8 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username}`;
 
 usercard.appendChild(p);
 
